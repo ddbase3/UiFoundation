6358b013714a62768b9637168de0e1020d2b357b
Daniel Dahme <danieldahme@googlemail.com>
2026-03-27 20:30:09 +0000

    header added

--- src/Api/IAdminDisplay.php
+++ src/Api/IAdminDisplay.php
@@ -1,5 +1,20 @@
 <?php declare(strict_types=1);
 
+/***********************************************************************
+ * This file is part of UiFoundation for BASE3 Framework.
+ *
+ * UiFoundation extends the BASE3 framework with a shared API
+ * foundation for user interface semantics and contracts.
+ * It provides reusable, framework-agnostic UI interfaces.
+ *
+ * Developed by Daniel Dahme
+ * Licensed under GPL-3.0
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * https://base3.de/v/uifoundation
+ * https://github.com/ddbase3/UiFoundation
+ **********************************************************************/
+
 namespace UiFoundation\Api;
 
 use Base3\Api\IDisplay;
